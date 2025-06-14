5974436d1ad58e9ece8d61933e7c42c4199ef9f4
Google <Google@gmail.com>
2025-06-14 17:57:57 +0300

    not tested but maybe done

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -18,14 +18,24 @@ $(document).ready(function () {
     });
 
     $(".city-name").on("change", function () {
+        const city = $(this).val(); 
         $.ajax({
-            type: "method",
-            url: "url",
-            data: "data",
-            dataType: "dataType",
-            success: function (response) {
-                
+            type: "GET",
+            url: `/get/${city}`,
+            dataType: "json", 
+            success: function (res) {
+                $(".weather").html(`
+                    <h2>${res.city}</h2>
+                    <p>${res.temp} °C, ${res.description}</p>
+                    <p>Швидкість вітру: ${res.windSpeed} м/с</p>
+                    <img src="http://openweathermap.org/img/wn/${res.img}@2x.png" alt="Погода іконка" />
+                `);
+            },
+            error: function(err) {
+                $(".weather").html(`<p>Помилка при завантаженні погоди</p>`);
+                console.error(err);
             }
         });
     });
+    
 });
