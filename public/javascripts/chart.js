877316506cef1d145050b110c0b6d2dbf0856136
Igorshk10 <igorshk271@gmail.com>
2025-06-27 14:07:40 +0300

    last version

--- public/javascripts/chart.js
+++ public/javascripts/chart.js
@@ -22,7 +22,9 @@ async function fetchCurrentWeather(city) {
     return {
         temp: data.main.temp,
         condition: data.weather[0].description,
-        name: data.name
+        name: data.name,
+        humidity: data.main.humidity,
+        wind: data.wind.speed,
     };
 }
 
@@ -43,8 +45,6 @@ function groupByDay(list) {
             temp: item.main.temp,
             time: `${date.getHours()}:00`,
             condition: item.weather[0].description,
-            humidity: item.main.humidity,
-            wind: item.wind.speed,
         });
     });
     return Object.entries(days).slice(0, 6);
@@ -70,7 +70,7 @@ function createChart(ctx, labels, data) {
 }
 
 async function renderCurrentWeather(city) {
-    const { temp, condition, name } = await fetchCurrentWeather(city);
+    const { temp, condition, name, humidity, wind } = await fetchCurrentWeather(city);
     const icon = getWeatherIcon(condition);
 
     const container = document.getElementById('current-container');
@@ -88,8 +88,12 @@ async function renderCurrentWeather(city) {
       <img src="${icon.static}" data-hover="${icon.animated}" class="weather-icon-hover">
       <h1 id="current" width="100" height="50"></h1>
       <div class="wind_humidity">
-       <p id="wind"></p>
-       <p id="humidity"></p>
+       <p id="wind"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-wind" viewBox="0 0 16 16">
+  <path d="M12.5 2A2.5 2.5 0 0 0 10 4.5a.5.5 0 0 1-1 0A3.5 3.5 0 1 1 12.5 8H.5a.5.5 0 0 1 0-1h12a2.5 2.5 0 0 0 0-5m-7 1a1 1 0 0 0-1 1 .5.5 0 0 1-1 0 2 2 0 1 1 2 2h-5a.5.5 0 0 1 0-1h5a1 1 0 0 0 0-2M0 9.5A.5.5 0 0 1 .5 9h10.042a3 3 0 1 1-3 3 .5.5 0 0 1 1 0 2 2 0 1 0 2-2H.5a.5.5 0 0 1-.5-.5"/>
+</svg> ${wind} m/s</p>
+       <p id="humidity"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-droplet-fill" viewBox="0 0 16 16">
+  <path d="M8 16a6 6 0 0 0 6-6c0-1.655-1.122-2.904-2.432-4.362C10.254 4.176 8.75 2.503 8 0c0 0-6 5.686-6 10a6 6 0 0 0 6 6M6.646 4.646l.708.708c-.29.29-1.128 1.311-1.907 2.87l-.894-.448c.82-1.641 1.717-2.753 2.093-3.13"/>
+</svg> ${humidity}</p>
       </div>
     </div>
   `;
