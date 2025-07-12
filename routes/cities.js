99b5e6058ea903b4fc56ad7b9da208f73054df1d
Артем Шабуневич <wouhnyua@gmail.com>
2025-07-12 11:54:05 +0300

    add quires and service

--- routes/cities.js
+++ routes/cities.js
@@ -1,29 +1,52 @@
-let express = require('express');
-let router = express.Router();
-let citiesService = require('../services/citiesService');
-
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
 
 router.get('/', async (req, res) => {
-    const query = req.query.q;
-    if (!query) return res.status(400).json({ error: 'Потрібен параметр q' });
     try {
-        let cities = await citiesService.getCities(query)
-        res.json(cities);
-    } catch (err) {
-        res.status(500).json({ error: 'Помилка при пошуку міста' });
+        const { q } = req.query;
+        const response = await axios.get('https://api.api-ninjas.com/v1/city', {
+            params: { name: q },
+            headers: {
+                'X-Api-Key': process.env.API_NINJA_KEY
+            }
+        });
+        res.json(response.data);
+    } catch (error) {
+        console.error('Error from API Ninjas:', error.message);
+        res.status(500).json([]);
     }
 });
-try {
-    const { q } = req.query;
-    const response = await axios.get('https://api.api-ninjas.com/v1/city', {
-        params: { name: q },
-        headers: {
-            'X-Api-Key': process.env.API_NINJA_KEY
-        }
-    });
-    res.json(response.data);
-} catch (error) {
-    console.error('Error from API Ninjas:', error.message);
-    res.status(500).json([]);
-}
+
 module.exports = router;
+
+//let express = require('express');
+// let router = express.Router();
+// let citiesService = require('../services/citiesService');
+//
+//
+// router.get('/', async (req, res) => {
+//     const query = req.query.q;
+//     if (!query) return res.status(400).json({ error: 'Потрібен параметр q' });
+//     try {
+//         let cities = await citiesService.getCities(query)
+//         res.json(cities);
+//     } catch (err) {
+//         res.status(500).json({ error: 'Помилка при пошуку міста' });
+//     }
+// });
+// try {
+//     const { q } = req.query;
+//     const response =  axios.get('https://api.api-ninjas.com/v1/city', {
+//         params: { name: q },
+//         headers: {
+//             'X-Api-Key': process.env.API_NINJA_KEY
+//         }
+//     });
+//     res.json(response.data);
+// } catch (error) {
+//     console.error('Error from API Ninjas:', error.message);
+//     res.status(500).json([]);
+// }
+// module.exports = router;//
+
